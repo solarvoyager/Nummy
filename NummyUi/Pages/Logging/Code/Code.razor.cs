644f6d96a1ff0332303b82f7f8551a8125bbc984
Hamid Musayev <hemidvsmusayev@gmail.com>
2024-01-15 17:36:27 +0400

    sorting fixes

--- NummyUi/Pages/Logging/Code/Code.razor.cs
+++ NummyUi/Pages/Logging/Code/Code.razor.cs
@@ -14,19 +14,20 @@ namespace NummyUi.Pages.Logging.Code;
 public partial class Code
 {
     [Inject] private ILogService LogService { get; set; }
+    [Inject] private ModalService ModalService { get; set; }
 
-    private CodeLogToListDto[]? _codeLogs;
+    private CodeLogToListDto[]? _items;
 
     private int _pageIndex = 1;
-    private int _pageSize = 5;
+    private int _pageSize = 10;
     private int _total = 0;
     private bool _loading;
-    
+
     private string _query = string.Empty;
     private SortOrder _sortOrder = SortOrder.Ascending;
-    private CodeLogSortType? _sortType = null;
+    private CodeLogSortType? _sortType;
 
-    private Dictionary<CodeLogLevel, bool> _logLevelFilters = new()
+    private readonly Dictionary<CodeLogLevel, bool> _logLevelFilters = new()
     {
         { CodeLogLevel.Trace, true },
         { CodeLogLevel.Debug, true },
@@ -55,7 +56,7 @@ public partial class Code
 
         await LoadDataAsync();
     }
-    
+
     private async Task OnSearch()
     {
         if (!string.IsNullOrWhiteSpace(_query))
@@ -69,20 +70,20 @@ public partial class Code
         _sortOrder = sortOrder;
         await LoadDataAsync();
     }
-    
+
     private async Task OnSortTypeChanged(CodeLogSortType sortType)
     {
         _sortType = sortType;
         await LoadDataAsync();
     }
-    
+
     private async Task LoadDataAsync()
     {
         _loading = true;
         StateHasChanged();
 
         List<CodeLogLevel> pickedLevels = [];
-        _logLevelFilters.Where(l=>l.Value).ForEach(l=>pickedLevels.Add(l.Key));
+        _logLevelFilters.Where(l => l.Value).ForEach(l => pickedLevels.Add(l.Key));
 
         var request = new GetCodeLogsRequestDto(
             _pageSize,
@@ -91,13 +92,64 @@ public partial class Code
             _sortType,
             _sortOrder,
             pickedLevels);
-            
+
         var result = await LogService.GetCodeLogs(request);
 
-        _codeLogs = result.Datas.ToArray();
+        _items = result.Datas.ToArray();
         _total = result.TotalCount;
 
         _loading = false;
         StateHasChanged();
+    }
+
+    private void ShowViewModal(Guid id)
+    {
+        var currentItem = _items!.First(i => i.Id == id);
+        
+        RenderFragment content = builder =>
+        {
+            builder.OpenComponent<Text>(1);
+            //builder.AddAttribute(2, "Style", "font-size: 29px");
+            builder.AddContent(1,  "<p>Text</>");
+            builder.CloseComponent();
+        };
+        
+        ModalService.Info(new ConfirmOptions
+        {
+            Title = $"{currentItem.Title}",
+            //Icon =  ,
+            Content = content,
+            OnCancel = (e) =>
+            {
+                Console.WriteLine("Ok");
+                return Task.CompletedTask;
+            }
+        });
+    }
+
+    private void ShowDeleteModal()
+    {
+        Func<ModalClosingEventArgs, Task> onOk = (e) =>
+        {
+            Console.WriteLine("Ok");
+            return Task.CompletedTask;
+        };
+
+        Func<ModalClosingEventArgs, Task> onCancel = (e) =>
+        {
+            Console.WriteLine("Cancel");
+            return Task.CompletedTask;
+        };
+
+
+        ModalService.Confirm(new ConfirmOptions()
+        {
+            Title = "Are you sure delete this task?",
+            //Icon =  ,
+            Content = "Some descriptions",
+            OnOk = onOk,
+            OnCancel = onCancel,
+            OkType = "danger",
+        });
     }
 }
