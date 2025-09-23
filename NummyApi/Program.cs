581340f611bce7e855f72a5115a3cbb6b5fa947e
hamidmusayev <hemidvsmusayev@gmail.com>
2025-09-24 02:21:35 +0400

    container name fixes, configurable super admin seed on startup, automatic migration on startup

--- NummyApi/Program.cs
+++ NummyApi/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NummyApi.DataContext;
+using NummyApi.Entitites;
+using NummyApi.Helpers;
 using NummyApi.Mappers;
 using NummyApi.Services.Abstract;
 using NummyApi.Services.Concrete;
@@ -19,7 +21,7 @@ var pass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
 
 var connectionString =
     $"Host={host};Port=5432;Database={db};Username={user};Password={pass};IncludeErrorDetail=true;";
-    //$"Host=localhost;Port=5432;Database=nummydatabase;Username=nummyuser;Password=nummypassword;IncludeErrorDetail=true;";
+//$"Host=localhost;Port=5432;Database=nummydatabase;Username=nummyuser;Password=nummypassword;IncludeErrorDetail=true;";
 
 // example for testing:
 // docker run --name postgres-container -e POSTGRES_PASSWORD=nummypassword -e POSTGRES_USER=nummyuser -p 5432:5432 -d postgres:latest
@@ -44,6 +46,36 @@ builder.Services.AddScoped<ITeamService, TeamService>();
 builder.Services.AddScoped<IApplicationService, ApplicationService>();
 
 var app = builder.Build();
+
+// Seed on startup
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<NummyDataContext>();
+    await context.Database.MigrateAsync(); // applies migrations
+
+    var email = Environment.GetEnvironmentVariable("UI_SUPER_ADMIN_EMAIL");
+    var password = Environment.GetEnvironmentVariable("UI_SUPER_ADMIN_PASSWORD");
+
+    var (hash, salt) = SecurityHelper.GeneratePasswordHash(password!);
+
+    var exists = await context.Users.AnyAsync(u => u.Email == email);
+    if (!exists)
+    {
+        context.Users.Add(new User
+        {
+            Id = new Guid("11111111-1111-1111-1111-111111111111"),
+            Name = "Super",
+            Surname = "Admin",
+            Email = email!,
+            PasswordHash = hash,
+            PasswordSalt = salt,
+            AvatarColorHex = "#4287f5",
+            CreatedAt = new DateTime(2025, 1, 1),
+        });
+        await context.SaveChangesAsync();
+    }
+}
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
