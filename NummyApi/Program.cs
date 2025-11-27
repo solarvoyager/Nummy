799954cc19fa153f07c198c7b28a4a7dbcb27c15
Hamid Musayev <68116432+HamidMusayev@users.noreply.github.com>
2025-11-27 21:14:54 +0400

    simple port fix

--- NummyApi/Program.cs
+++ NummyApi/Program.cs
@@ -21,7 +21,7 @@ var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
 var pass = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
 
 var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={pass};IncludeErrorDetail=true;";
-//$"Host=localhost;Port=5432;Database=nummydatabase;Username=nummyuser;Password=nummypassword;IncludeErrorDetail=true;";
+//$"Host=localhost;Port=5433;Database=nummydatabase;Username=nummyuser;Password=nummypassword;IncludeErrorDetail=true;";
 
 // example for testing:
 // docker run --name postgres-container -e POSTGRES_PASSWORD=nummy_password -e POSTGRES_USER=nummy_user -p 5433:5432 -d postgres:latest
